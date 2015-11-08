7e285c3fb88ec2b1675ec0b4c0a77776e20c5dc9
Narr <nardgu@gmail.com>
2015-11-08 15:55:48 +0900

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,35 @@ module.exports = {
   },
   module: {
     loaders: [{
+      test: /\.css$/,
+      loader: "style!css"
+    }, {
       test: /\.scss$/,
       loader: 'style!css!sass'
     }, {
       test: /\.js$/,
       loader: 'ng-annotate!babel?presets[]=es2015!jshint',
       exclude: /node_modules|bower_components/
+    }, {
+      test: /\.html/,
+      loader: 'raw'
+    }, {
+      test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
+      loader: 'file-loader?name=res/[name].[ext]?[hash]'
+    }, {
+      test: /\.json/,
+      loader: 'json'
     }]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      MODE: {
+        production: process.env.NODE_ENV === 'production'
+      }
+    })
+  ],
+  resolve: {
+    root: APP
+  }
 };
