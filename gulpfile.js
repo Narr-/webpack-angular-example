48a861d5659c68a158116fabb757851a9f957ea9
Narr <nardgu@gmail.com>
2015-11-17 18:07:37 +0900

    Done Base version

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,81 @@
 'use strict';
 
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var webpack = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
-var webpackConfig = require("./webpack.config.js");
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var del = require('del');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config.js');
 
+////////////////////////
 // The development server (the recommended option for development)
-gulp.task("webpack-dev-server", function(callback) {
-  // modify some webpack config options
-  var myConfig = Object.create(webpackConfig);
-  myConfig.devtool = "sourcemap";
-  // myConfig.debug = true;
+gulp.task('webpack-dev-server', function(done) {
+  var port = 8080;
+  // Modify some webpack config options
+  var devConfig = Object.create(webpackConfig);
+  devConfig.entry.unshift('webpack-dev-server/client?http://localhost:' + port); // for Inline mode
+  devConfig.plugins = devConfig.plugins.concat(
+    new webpack.DefinePlugin({
+      MODE: {}
+    })
+  );
+  devConfig.devtool = 'source-map';
+  devConfig.debug = true; // Switch loaders to debug mode.
 
   // Start a webpack-dev-server
-  new WebpackDevServer(webpack(myConfig), {
-    contentBase: __dirname + "/app",
+  new WebpackDevServer(webpack(devConfig), {
     stats: {
       colors: true
     }
-  }).listen(8080, "localhost", function() {
-    gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+  }).listen(port, 'localhost', function(err) {
+    if (err) {
+      throw new gutil.PluginError('webpack-dev-server', err);
+    }
+    gutil.log('[webpack-dev-server:Inline Mode]', 'http://localhost:' + port);
+    gutil.log('[webpack-dev-server:Iframe Mode]', 'http://localhost:' + port + '/webpack-dev-server/index.html');
   });
 });
 
-gulp.task("default", ["webpack-dev-server"]);
+gulp.task('default', ['webpack-dev-server']);
 
-// Build and watch cycle (another option for development)
-// Advantage: No server required, can run app from filesystem
-// Disadvantage: Requests are not blocked until bundle is available,
-//               can serve an old app on refresh
-gulp.task("build-dev", ["webpack:build-dev"], function() {
-  gulp.watch(["app/**/*"], ["webpack:build-dev"]);
-});
-
+////////////////////////
 // Production build
-gulp.task("build", ["webpack:build"]);
+gulp.task('webpack:build', function(done) {
+  del.sync('dist');
 
-gulp.task("webpack:build", function(callback) {
   // modify some webpack config options
-  var myConfig = Object.create(webpackConfig);
-  // myConfig.plugins = myConfig.plugins.concat(
-  //   new webpack.DefinePlugin({
-  //     "process.env": {
-  //       // This has effect on the react lib size
-  //       "NODE_ENV": JSON.stringify("production")
-  //     }
-  //   }),
-  //   new webpack.optimize.DedupePlugin(),
-  //   new webpack.optimize.UglifyJsPlugin()
-  // );
+  var config = Object.create(webpackConfig);
+  config.bail = true; // Report the first error as a hard error instead of tolerating it.
+  config.plugins = config.plugins.concat(
+    new webpack.optimize.DedupePlugin(), // https://github.com/webpack/docs/wiki/optimization#deduplication
+    new webpack.optimize.UglifyJsPlugin({
+      // beautify: true,
+      // mangle: false
+    }),
+    new webpack.DefinePlugin({
+      MODE: {
+        production: true
+      }
+    })
+  );
+  // config.devtool = 'source-map';
 
-  // run webpack
-  webpack(myConfig, function(err, stats) {
+  // create a single instance of the compiler
+  var compiler = webpack(config);
+  // run
+  compiler.run(function(err, stats) {
     if (err) {
-      throw new gutil.PluginError("webpack:build", err);
+      console.log(err.module.resource); // show the file name where error occured
+      if (err.module.errors.length > 0) {
+        console.log(err.module.errors[0].error); // show the error detail e.g. jshint or jscs loader
+      }
+      throw new gutil.PluginError('webpack:build', err);
     }
-    gutil.log("[webpack:build]", stats.toString({
+    gutil.log('[webpack:build]', stats.toString({
       colors: true
     }));
-    callback();
+    done();
   });
 });
 
-// modify some webpack config options
-var myDevConfig = Object.create(webpackConfig);
-myDevConfig.devtool = "sourcemap";
-myDevConfig.debug = true;
-
-// create a single instance of the compiler to allow caching
-var devCompiler = webpack(myDevConfig);
-
-gulp.task("webpack:build-dev", function(callback) {
-  // run webpack
-  devCompiler.run(function(err, stats) {
-    if (err) {
-      throw new gutil.PluginError("webpack:build-dev", err);
-    }
-    gutil.log("[webpack:build-dev]", stats.toString({
-      colors: true
-    }));
-    callback();
-  });
-});
+gulp.task('build', ['webpack:build']);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   context: appPath, // for resolving the entry option
   entry: [
-    'webpack/hot/dev-server',
+    // 'webpack-dev-server/client?http://localhost:8080', // for Inline mode
     './core/bootstrap.js'
   ],
   output: {
@@ -16,30 +16,46 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'jscs!jshint'
+    }],
     loaders: [{
       test: /\.css$/,
-      loader: "style!css"
+      loader: 'style!css'
+    }, {
+      test: /\.html$/,
+      loader: 'ngtemplate?module=todomvc&relativeTo=' + appPath + '&prefix=partials!html'
+    }, { // TODO
+      test: /\.(woff|woff2|ttf|eot|svg)(\?.*?)?$/,
+      loader: 'file-loader?name=res/[name].[ext]?[hash]'
     }, {
       test: /\.js$/,
-      loader: 'ng-annotate!jshint',
-      exclude: /node_modules|bower_components/
-    }, {
-      test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
-      loader: 'file-loader?name=res/[name].[ext]?[hash]'
+      loader: 'ng-annotate',
+      exclude: /node_modules/
     }]
   },
+  // more options in the optional jshint object
+  jshint: {
+    // errors are displayed by default as warnings
+    // set emitErrors to true to display them as errors
+    emitErrors: true,
+    // to not interrupt the compilation
+    // if you want any file with jshint errors to fail
+    // set failOnHint to true
+    failOnHint: true
+  },
+  jscs: {
+    emitErrors: true,
+    failOnHint: true
+  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      MODE: {
-        production: process.env.NODE_ENV === 'production'
-      }
-    }),
     new HtmlWebpackPlugin({
-      template: appPath + '/index.html'
+      template: appPath + '/index.tmpl' // to avoid conflict with ngtemplate loader, change extension of the index to tmpl
     })
   ],
   resolve: {
-    root: appPath
+    root: appPath // for require
   }
 };
