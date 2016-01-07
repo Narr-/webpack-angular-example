4dcbd86045f0304b3c942f1058309073b7f2f8e6
Narr <nardgu@gmail.com>
2016-01-08 00:14:31 +0900

    modify jshint errors on index.js

--- server/config/index.js
+++ server/config/index.js
@@ -6,13 +6,13 @@ var redisUrl;
 var mongoUri;
 
 if (env.DOCKER_ENV) {
-  redisUrl = 'redis://redis:6379'
+  redisUrl = 'redis://redis:6379';
   mongoUri = 'mongodb://mongo:27017/todoDb';
 } else if (env.DYNO) { // HEROKU
-  redisUrl = env.REDIS_URL
-  mongoUri = env.MONGOLAB_URI
+  redisUrl = env.REDIS_URL;
+  mongoUri = env.MONGOLAB_URI;
 } else {
-  redisUrl = 'redis://localhost:6379'
+  redisUrl = 'redis://localhost:6379';
   mongoUri = 'mongodb://localhost:27017/todoDb';
 }
 
